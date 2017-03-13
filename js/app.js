db3f254b6a1417a6d8387481c6574715f1dd6ac8
themartinsjoseph <themartinsjoseph@gmail.com>
2017-03-13 16:43:19 -0700

    completed the first two recommended steps

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 console.log('app.js was loaded.');
+var calcApp = angular.module("AngularCalcApp", []);
+calcApp.controller('HomeCtrl', ['$scope', function($scope){
+	$scope.value = '0';
+
+	// Get buttons to update display
+	// Get clear button to clear display
+
+	$scope.toggleValue = function(n){
+	if ($scope.value == '0') {
+			$scope.value = n;
+		} else {
+			$scope.value = '0';
+		}
+	};
+
+
+
+
+
+
+}]);
+
+
+
