050cfd3169bb9235f9786522c59d55b969d58b11
dddotcom <dddotcom@gmail.com>
2017-03-13 22:56:21 -0700

    redid code as calculator wasn't working for some cases

--- js/app.js
+++ js/app.js
@@ -3,32 +3,38 @@ console.log('app.js was loaded.');
 var calcApp = angular.module("calcApp", []);
 
 calcApp.controller("CalcCtrl", ["$scope", function($scope){
-  $scope.value = '0';
-  $scope.numbers = [];
+  $scope.display = '0';
+  $scope.total = 0;
   $scope.operator = '';
+  $scope.append = false;
+  
+  $scope.initialize = function(){
+    $scope.display = '0';
+    $scope.total = 0;
+    $scope.operator = '';
+    $scope.append = false;
+  };
 
   $scope.buttonPush = function(event){
-    if($scope.value == '0' || $scope.value == ''){
-      $scope.value = event.target.textContent;
-    } else if($scope.operator !== ''){
-      $scope.value = event.target.textContent;
+    if(!$scope.operator){
+      if($scope.display === '0'){
+        $scope.display = event.target.textContent;
+      } else {
+        $scope.display += event.target.textContent;
+      }
     } else {
-      $scope.value += event.target.textContent;
+      if($scope.append){
+        $scope.display += event.target.textContent;
+      } else {
+        $scope.append = true;
+        $scope.display = event.target.textContent;
+      }
     }
   };
 
   $scope.operatorPush = function(event){
-    console.log("operator pushed");
     //store the value in the calculator
-    $scope.numbers.push($scope.value);
-
-    //check if we need to evaluate it it
-    if($scope.numbers.length === 2){
-      $scope.getIntermediateAnswer();
-    } else {
-      //clear the value
-      $scope.value = '';
-    }
+    $scope.getTotal();
 
     if(escape(event.target.textContent) == '%F7'){
       $scope.operator = '/';
@@ -43,25 +49,15 @@ calcApp.controller("CalcCtrl", ["$scope", function($scope){
     }
   };
 
-  $scope.getIntermediateAnswer = function(){
-    //evaluate the expression
-    var result = eval($scope.numbers[0] + $scope.operator + $scope.numbers[1]);
-    //display it on the calculator
-    $scope.value = result;
-    //clear the numbers array and add the new value to the array
-    $scope.numbers = [result];
+  $scope.getTotal = function(){
+    if($scope.operator){
+      $scope.total = eval($scope.total + $scope.operator + $scope.display);
+      $scope.display = $scope.total;
+      $scope.append = false;
+      $scope.operator = '';
+    } else {
+      $scope.total = $scope.display;
+    }
   };
 
-  $scope.getFinalAnswer = function(){
-    var result = eval($scope.numbers[0] + $scope.operator + $scope.value);
-    $scope.value = result;
-    $scope.numbers = [];
-    $scope.operator = '';
-  }
-
-  $scope.clear = function(){
-    $scope.numbers = [];
-    $scope.operator = '';
-    $scope.value = '0';
-  };
 }]);
