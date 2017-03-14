956b879b07a5a85c887be7f8ec41eb007fda319d
dddotcom <dddotcom@gmail.com>
2017-03-14 08:56:28 -0700

    requirement If a number is entered without an operator being pressed first it should clear the screen / memory and that new number should become the running total now works

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ calcApp.controller("CalcCtrl", ["$scope", function($scope){
   $scope.total = 0;
   $scope.operator = '';
   $scope.append = false;
-  
+
   $scope.initialize = function(){
     $scope.display = '0';
     $scope.total = 0;
@@ -16,12 +16,18 @@ calcApp.controller("CalcCtrl", ["$scope", function($scope){
   };
 
   $scope.buttonPush = function(event){
+    console.log($scope.operator);
     if(!$scope.operator){
+        //TODO: If a number is entered without an operator being pressed first it should
+        //  clear the screen / memory and that new number should become the running total
       if($scope.display === '0'){
         $scope.display = event.target.textContent;
       } else {
         $scope.display += event.target.textContent;
       }
+    } else if ($scope.operator === '='){
+      $scope.display = event.target.textContent;
+      $scope.operator = '';
     } else {
       if($scope.append){
         $scope.display += event.target.textContent;
@@ -34,16 +40,21 @@ calcApp.controller("CalcCtrl", ["$scope", function($scope){
 
   $scope.operatorPush = function(event){
     //store the value in the calculator
-    $scope.getTotal();
+    console.log(escape(event.target.textContent));
+    if($scope.operator !== '='){
+        $scope.getTotal();
+    }
 
-    if(escape(event.target.textContent) == '%F7'){
+    if(escape(event.target.textContent) ==='%F7'){
       $scope.operator = '/';
     }
-    else if(escape(event.target.textContent) == '%D7'){
+    else if(escape(event.target.textContent) === '%D7'){
       $scope.operator = '*';
     }
-    else if(escape(event.target.textContent) == '%u2212'){
+    else if(escape(event.target.textContent) === '%u2212'){
       $scope.operator = '-';
+    } else if (escape(event.target.textContent) === '%3D'){
+      $scope.operator = '=';
     } else {
       $scope.operator = '+';
     }
@@ -54,7 +65,6 @@ calcApp.controller("CalcCtrl", ["$scope", function($scope){
       $scope.total = eval($scope.total + $scope.operator + $scope.display);
       $scope.display = $scope.total;
       $scope.append = false;
-      $scope.operator = '';
     } else {
       $scope.total = $scope.display;
     }
