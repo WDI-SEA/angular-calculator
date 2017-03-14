36230a7e5c26eb67869737dafbd14d96e01b1f9c
m-woo <mark.chi.woo@gmail.com>
2017-03-14 09:03:56 -0700

    addd clear button

--- js/app.js
+++ js/app.js
@@ -4,14 +4,17 @@ var introApp = angular.module('Calculator', []);
 
 
 introApp.controller('homeCtrl', ['$scope', function($scope){
-	
+	// Bind to output display
 	$scope.output = '0';
 
 	$scope.newNumber = true;
-
+	//holds number value of the string in the display output
     $scope.pendingValue = null;
 
-
+    var ADD = 'adding';
+    var SUBTRACT = 'subtracting';
+    var ADD_TOKEN = '+';
+    var SUBTRACT_TOKEN = '-';
 
 
 	$scope.calc = function(btn){
@@ -25,13 +28,22 @@ introApp.controller('homeCtrl', ['$scope', function($scope){
 		$scope.pendingValue = toNumber($scope.output);
 	};
 
-	 toNumber = function(numberString) {
-	    var result = 0;
-	    if(numberString) {
-	      result = numberString*1;
-	    }
-	    return result;
-	  };
+	toNumber = function(numberString) {
+    	var result = 0;
+    	if(numberString) {
+      		result = numberString*1;
+    	}
+    	return result;
+  	};
+  	$scope.onClickClear = function(){
+  		$scope.output = '0';
+  	};
+  	// $scope.onClickAdd = function(){
+  	// 	$scope.output +=  
+  	// };
+
+
+
 
 
 
