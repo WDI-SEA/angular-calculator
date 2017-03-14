44da83a4f0655e57a78a6595eaa68928a3a020e8
krichdev <kgrichardson2@gmail.com>
2017-03-13 22:22:18 -0700

    first evaluation works, then clear button isn't clearing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 console.log('app.js was loaded.');
+var angularCalc = angular.module("angularCalc", []);
+
+angularCalc.controller("calcCtrl", ["$scope", function($scope){
+  var btnsClicked = '';
+
+  $scope.value = '0';
+
+  $scope.btnPressed = function(numberSymbol){
+    var lastNum = btnsClicked[btnsClicked.length - 1];
+
+    if($scope.value === '0') {
+      $scope.value = numberSymbol.toString();
+      btnsClicked += numberSymbol.toString();
+    } else if (numberSymbol === "+" || numberSymbol === "-" || numberSymbol === "/" || numberSymbol === "*"){
+      $scope.value = eval(btnsClicked);
+      btnsClicked += numberSymbol;
+    } else {
+      if (lastNum === "+" || lastNum === "-" || lastNum === "/" || lastNum === "*"){
+        $scope.value = numberSymbol.toString();
+      } else {
+        $scope.value += numberSymbol.toString();
+      }
+      btnsClicked += numberSymbol.toString();
+    }
+  };
+
+  $scope.clearBtn = function(){
+    $scope.value = '0';
+    var btnsClicked = '';
+  }
+
+  $scope.getTotal = function() {
+    $scope.value = eval(btnsClicked);
+  }
+
+}])
