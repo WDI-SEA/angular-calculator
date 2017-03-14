2cbe9e6cb4f404ed0f9161cad0a949a4cf9a5275
m-woo <mark.chi.woo@gmail.com>
2017-03-13 22:02:15 -0700

    followed online tutorial to get integers to appear in display

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
 console.log('app.js was loaded.');
+var introApp = angular.module('Calculator', []);
+
+
+
+introApp.controller('homeCtrl', ['$scope', function($scope){
+	
+	$scope.output = '0';
+
+	$scope.newNumber = true;
+
+    $scope.pendingValue = null;
+
+
+
+
+	$scope.calc = function(btn){
+		console.log(btn)
+		if($scope.output == '0' || $scope.newNumber){
+			$scope.output = btn;
+			$scope.newNumber = false;
+		} else {
+			$scope.output+= String(btn);
+		}
+		$scope.pendingValue = toNumber($scope.output);
+	};
+
+	 toNumber = function(numberString) {
+	    var result = 0;
+	    if(numberString) {
+	      result = numberString*1;
+	    }
+	    return result;
+	  };
+
+
+
+}])
