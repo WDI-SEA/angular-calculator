aa018c37e03d6e343bb63325717109b0b713f4b0
jadehartman <rachel.jade.hartman@gmail.com>
2017-05-08 22:15:15 -0700

    still working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log('app.js was loaded.');
+// console.log('app.js was loaded.');
+angular.module('app', [])
+    .controller("MyController", ['$scope', function($scope) {
+        $scope.numClick = function($event) {
+            var click = function() {}
+        }
+    }]);
+//controller is working!
