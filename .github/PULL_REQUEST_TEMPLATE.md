cf02a9d5be2fb846688bb55e8827bcb2ef885d0b
jadehartman <rachel.jade.hartman@gmail.com>
2017-05-08 23:01:00 -0700

    incomplete

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -3,7 +3,8 @@
 Please describe your comfort and completeness levels before submitting.
 
 Comfort Level (1-5): 
-
+0
 Completeness Level (1-5):
-
+1
 What did you think of this deliverable?:
+Too difficult, I dont get it. / I need to revisit javascript. What can I do?
--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 // console.log('app.js was loaded.');
 angular.module('app', [])
-    .controller("MyController", ['$scope', function($scope) {
-        $scope.numClick = function($event) {
-            var click = function() {}
-        }
+    .controller("MyController", ["$scope", function($scope) {
+        $scope.num1 = undefined;
+        $scope.num2 = undefined;
+        $scope.operator = undefined;
+        $scope.display = '0';
+
+        // if 0 is present change the display to the number of numClick
+        $scope.numClick = function(number) {
+            if ($scope.display == '0') {
+                $scope.display = num.toString();
+            } else {
+                $scope.display += digit.toString();
+
+
+            }
+        };
+
+        $scope.operatorClick = function(operator) {
+            console.log('this is the operator' + operator);
+        };
     }]);
-//controller is working!
