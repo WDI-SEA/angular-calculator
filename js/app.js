1da3fe5c0125cab5b7b6264725ddd0cceedf0b48
jigglor <andrewtsuji@gmail.com>
2017-05-08 21:50:11 -0700

    its a calculator

--- js/app.js
+++ js/app.js
@@ -1,1 +1,64 @@
+/* globals angular */
 console.log('app.js was loaded.');
+
+var calcApp = angular.module('Calculon', []);
+calcApp.controller('Calculator', ['$scope', function($scope) {
+    $scope.bankA = undefined;
+    $scope.operator = undefined;
+    $scope.screen = '0';
+    $scope.cleared = true;
+
+    $scope.pressNumber = function(num) {
+        if ($scope.cleared === true) {
+            $scope.cleared = false;
+            $scope.screen = '';
+            $scope.screen += num;
+        } else {
+            $scope.screen += num;
+        }
+    };
+
+    $scope.pressOperator = function(op) {
+        if ($scope.bankA === undefined) {
+            $scope.bankA = parseInt($scope.screen);
+            $scope.screen = '';
+        }
+        equalFunct();
+        $scope.screen = $scope.bankA.toString();
+        $scope.cleared = true;
+        $scope.operator = op;
+    };
+
+    $scope.pressEqual = function() {
+        equalFunct();
+        clearInputs();
+    };
+
+    $scope.clear = function() {
+        clearInputs();
+        $scope.cleared = true;
+        $scope.screen = '0';
+    };
+
+    function equalFunct() {
+        switch ($scope.operator) {
+            case '+':
+                $scope.bankA = parseInt($scope.screen) + $scope.bankA;
+                break;
+            case '-':
+                $scope.bankA = ($scope.bankA - parseInt($scope.screen)).toString();
+                break;
+            case '/':
+                $scope.bankA = ($scope.bankA / parseInt($scope.screen)).toString();
+                break;
+            case '*':
+                $scope.bankA = (parseInt($scope.screen) * $scope.bankA).toString();
+                break;
+        }
+    }
+
+    function clearInputs() {
+        $scope.bankA = undefined;
+        $scope.operator = undefined;
+    }
+}]);
