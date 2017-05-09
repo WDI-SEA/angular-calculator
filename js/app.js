fa583429ec8a9d08375fffdfbcff1f2de508f887
buffalocheck <shawn@buffalocheck.com>
2017-05-08 19:35:05 -0700

    this is where we got to

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 console.log('app.js was loaded.');
+angular.module("app", [])
+    .controller("MyController", ["$scope", function($scope) {
+        $scope.display = "0";
+        $scope.num1 = 0;
+        $scope.operator = '';
+
+        $scope.numClick = function(digit) {
+            //if 0 is diplayed then change display to the numClick value, called digit
+            if ($scope.display == "0") {
+                $scope.display = digit;
+            } else {
+                $scope.display += digit.toString();
+            }
+        };
+
+        $scope.opClick = function(operator) {
+            console.log("Operator is " + operator);
+            $scope.num1 = $scope.display;
+            $scope.operator = operator;
+        };
+
+    }]);
